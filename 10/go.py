a9334a77d0020f2ff9225dc6620d25f0c6ccce7a
Tim Courrejou <timcour@users.noreply.github.com>
2021-12-10 11:38:40 -0800

    day-10 refactor

--- 10/go.py
+++ 10/go.py
@@ -16,40 +16,25 @@ O2C = {}
 for k in M:
     O2C[M[k]['o']] = k
 
+# Returns the stack and first mismatched token if corrupt
+def parse(tokens):
+    stack = []
+    for t in tokens:
+        if t in openers:
+            stack.append(t)
+            continue
+        if M[t]['o'] == stack[-1]:
+            stack.pop()
+        else:
+            return stack, t
+    return stack, None
 
 def find_score(tokens):
-    stack = []
-    for t in tokens:
-        if t in openers:
-            stack.append(t)
-            continue
-        if M[t]['o'] == stack[-1]:
-            stack.pop()
-        else:
-            return M[t]['score']
-    return 0
-
-def is_corrupt(tokens):
-    stack = []
-    for t in tokens:
-        if t in openers:
-            stack.append(t)
-            continue
-        if M[t]['o'] == stack[-1]:
-            stack.pop()
-        else:
-            return True
-    return False
+    stack, t = parse(tokens)
+    return M[t]['score'] if t else 0
 
 def completion_score(tokens):
-    stack = []
-    for t in tokens:
-        if t in openers:
-            stack.append(t)
-            continue
-        if M[t]['o'] == stack[-1]:
-            stack.pop()
-
+    stack, t = parse(tokens)
     score = 0
     while stack:
         o = stack.pop()
@@ -58,26 +43,25 @@ def completion_score(tokens):
     return score
 
 def find_winner(lines):
-    lines = filter(lambda x: not is_corrupt(x), lines)
-
+    lines = filter(lambda x: find_score(x) == 0, lines)
     scores = []
     for line in lines:
         s = completion_score(line)
         scores.append(s)
-        print('score', s)
-
     return sorted(scores)[int(len(scores) / 2)]
+
+def find_corrupt_score(lines):
+    total = 0
+    for line in lines:
+        s = find_score(line)
+        total += s
+    return total
 
 if __name__=='__main__':
     lines = []
     for line in sys.stdin.readlines():
         lines.append(list(line.strip()))
-    print('lines', lines)
-    # total = 0
-    # for line in lines:
-    #     s = find_score(line)
-    #     total += s
-    #     print('score', s)
-    # print('total', total)
+    total = find_corrupt_score(lines)
+    print('corrupt score:', total)
     res = find_winner(lines)
-    print('res', res)
+    print('autocomplete score:', res)
